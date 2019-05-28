0093eeb5cf841210d214b63e99a96f2a4ee0d676
harshith714 <51110054+harshith714@users.noreply.github.com>
2019-05-28 12:25:08 +0530

    Add files via upload

---
+++ Enumeration.java
@@ -0,0 +1,23 @@
+public class Enumeration{
+	enum Color{
+		RED,BLUE,GREEN, YELLOW;
+	}
+
+	public static void main(String[] args) {
+		Color c1= Color.RED;
+		System.out.println(c1);
+
+
+		
+
+
+
+
+
+
+
+
+
+		
+		}
+}
